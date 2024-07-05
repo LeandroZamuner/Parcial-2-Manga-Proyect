36f6d1d41c384e699549b0a9161322c4e9ae1efe
gerwalzer <german.walzer@davinci.edu.ar>
2024-07-04 22:06:15 -0300

    enviando Footer y Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Estilos CSS para el carousel
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const App = () => {
   return (
     <Carousel>
       <div>
         <img src="/images/Carousel1.jpg" alt="Imagen 1" />
-        <p className="legend">Texto descriptivo 1</p>
+        <p className="legend">Texto 1</p>
       </div>
       <div>
         <img src="/images/Carousel2.jpg" alt="Imagen 2" />
-        <p className="legend">Texto descriptivo 2</p>
+        <p className="legend">Texto 2</p>
       </div>
       <div>
         <img src="/images/Carousel3.jpg" alt="Imagen 3" />
-        <p className="legend">Texto descriptivo 3</p>
+        <p className="legend">Texto 3</p>
       </div>
     </Carousel>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-none text-white text-center py-3">
-      <p>&copy; 2024 Manga Tracker</p>
+    <footer className="bg-dark text-white py-4">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 text-center text-md-left">
+            <p>&copy; 2024 Manga Tracker</p>
+            <p>
+              Para más información, contáctanos:<br />
+              Teléfono: <a href="tel:+5401156596336" className="text-white">+54 011 5659 6336</a><br />
+              Facebook: <a href="https://www.facebook.com/MangaTracker" target="_blank" rel="noopener noreferrer" className="text-white">MangaTracker</a><br />
+              Instagram: <a href="https://www.instagram.com/MangaTracker" target="_blank" rel="noopener noreferrer" className="text-white">MangaTracker</a>
+            </p>
+          </div>
+          <div className="col-md-6 text-center text-md-right">
+            <img src="/images/LogoRana.png" alt="Logo Rana" style={{ maxWidth: '150px', maxHeight: '100px' }} />
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
