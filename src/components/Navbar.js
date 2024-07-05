9525e11ff13842a16af848a99f93e12000f7e9c8
gerwalzer <german.walzer@davinci.edu.ar>
2024-07-04 22:20:11 -0300

    Navbar ajustado y Mangaitem con restricciones

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,34 +4,45 @@ import { Navbar as BootstrapNavbar, Nav } from 'react-bootstrap';
 const CustomNavbar = ({ isAdmin, onLogin, onLogout, isLoggedIn }) => {
 
   return (
-
     <BootstrapNavbar bg="none" variant="dark" expand="lg">
+      {/* Logo a la izquierda */}
+      <BootstrapNavbar.Brand href="#home" style={{ paddingLeft: '20px' }}>
+        <img
+          src="/images/LogoRana.png"
+          height="30"
+          className="d-inline-block align-top"
+          alt="Logo"
+          style={{ marginRight: '10px' }}
+        />
+        Manga Tracker
+      </BootstrapNavbar.Brand>
 
-      <BootstrapNavbar.Brand style={{ paddingLeft: '20px' }} href="#home"> Manga Tracker </BootstrapNavbar.Brand >
-
+      {/* Botón de toggle para colapsar en móviles */}
       <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
 
+      {/* Contenido del Navbar colapsable */}
       <BootstrapNavbar.Collapse id="basic-navbar-nav">
-
+        {/* Links a la izquierda */}
         <Nav className="mr-auto">
-
           {isLoggedIn ? (
             <>
               <Nav.Link href="#calendar">Calendar</Nav.Link>
               {isAdmin && <Nav.Link href="#admin">User Management</Nav.Link>}
               <Nav.Link href="#favorites">Favorites</Nav.Link>
-              <Nav.Link onClick={onLogout}>Logout</Nav.Link>
             </>
+          ) : null}
+        </Nav>
+        
+        {/* Botón de login/logout a la derecha */}
+        <Nav className="ml-auto">
+          {isLoggedIn ? (
+            <Nav.Link onClick={onLogout}>Logout</Nav.Link>
           ) : (
             <Nav.Link onClick={onLogin}>Login</Nav.Link>
           )}
-
         </Nav>
-
       </BootstrapNavbar.Collapse>
-
     </BootstrapNavbar>
-    
   );
 };
 
